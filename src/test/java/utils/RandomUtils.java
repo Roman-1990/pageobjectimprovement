3b00b86abf621f70c2f7432d9c7448c23ebc5964
Roman <pcuxman@yandex.ru>
2021-06-09 19:14:25 +0300

    Удален метод рандомных символов2

--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -2,21 +2,12 @@ package utils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
-    //Образец рандомной белеберды
-    public static String getRandomString(int length) {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder result = new StringBuilder();
-        Random rnd = new Random();
-        while (result.length() < length) {
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            result.append(SALTCHARS.charAt(index));
-        }
-        return result.toString();
-    }
+
+
     //ввод рандомных данных по фрормам
     public static String getRandomGender() {
         List<String> list = Arrays.asList("Female", "Male", "Other");
