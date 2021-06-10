c0cd745116f4f116f7c28ea715b8101b51304fd6
Roman <pcuxman@yandex.ru>
2021-06-10 11:21:01 +0300

    delete random string

--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -6,16 +6,6 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
-    public static String getRandomString(int length) {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder result = new StringBuilder();
-        Random rnd = new Random();
-        while (result.length() < length) {
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            result.append(SALTCHARS.charAt(index));
-        }
-        return result.toString();
-    }
 
     public static String getRandomGender() {
         List<String> list = Arrays.asList("Female", "Male", "Other");
